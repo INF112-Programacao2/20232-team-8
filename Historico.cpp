cf980b60e3e0311a076f2684378ce6130697c3c5
lilicesm <alicesm3012@gmail.com>
2023-11-07 20:57:59 -0300

    Criei a classe Historico e defini os metodos

--- Historico.cpp
+++ Historico.cpp
@@ -1,3 +1,34 @@
 #include "historico.h"
 #include "vacina.h"
 #include "posto.h"
+
+Historico::Historico(std::string data, int dose, bool retorno, std::string dataDeRetorno, Vacina vacina_, Posto posto_):
+    _data(data), _dose(dose), _retorno(retorno), _dataDeRetorno(dataDeRetorno), _vacina_(vacina_), _posto_(posto_) {}
+
+std::string Historico::get_data(){
+    return _data;
+}
+
+int Historico::get_dose(){
+    return _dose;
+}
+
+void Historico::set_dose(int atualDose){
+    _dose = atualDose;
+}
+
+bool Historico::get_retorno(){
+    return _retorno;
+}
+
+void Historico::set_retorno(bool atualRetorno){
+    _retorno = atualRetorno;
+}
+
+std::string Historico::get_dataDeRetorno(){
+    return _dataDeRetorno;
+}
+
+void Historico::set_dataDeRetorno(std::string atualDdr){
+    _dataDeRetorno = atualDdr;
+}
--- Historico.h
+++ Historico.h
@@ -4,6 +4,25 @@
 #include "vacina.h"
 #include "posto.h"
 
+class Historico {
+private:
+    std::string _data;
+    int _dose;
+    bool _retorno;
+    std::string _dataDeRetorno;
+    Vacina _vacina_;
+    Posto _posto_;
 
+public: 
+    Historico(std::string data, int dose, bool retorno, std::string dataDeRetorno, Vacina vacina_, Posto posto_);
+
+    std::string get_data();
+    int get_dose();
+    void set_dose(int atualDose);
+    bool get_retorno();
+    void set_retorno(bool atualRetorno);
+    std::string get_dataDeRetorno();
+    void set_dataDeRetorno(std::string atualDdr);
+};
 
 #endif
