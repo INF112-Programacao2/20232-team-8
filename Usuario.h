d72a08a111552a77bd9bb9310cb342dd9df9e920
jucalia <jugalvaomatos@gmail.com>
2023-11-07 19:12:56 -0300

    Modificação na classe Usuario

--- Usuario.h
+++ Usuario.h
@@ -2,6 +2,17 @@
 #define INF112_USUARIO_H
 #include <string>
 
-
+class Usuario{
+private:
+    std::string _nome;
+    long long int _cpf;
+    std::string _datanascimento;
+    long long int _telefone;
+public:
+    std::string get_nome();
+    long long int get_cpf();
+    std::string get_datanascimento();
+    long long int _telefone();    
+};
 
 #endif
