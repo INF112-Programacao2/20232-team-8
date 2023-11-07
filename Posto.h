92e10043a52ffbc889878d33fe77f83f8e5a5444
cecilia <cecicassab@gmail.com>
2023-11-07 17:55:06 -0300

    Criei as variaveis de posto

--- Posto.h
+++ Posto.h
@@ -1,7 +1,17 @@
 #ifndef INF112_POSTO_H
 #define INF112_POSTO_H
 #include <string>
+#include "produto.h"
 
+class Posto {
+    private:
+    std::string _login;
+    int _senha;
+    std::string _endereco;
+    Produto _estoque;
 
+    public:
+    Posto(std::string _login, int _senha, std::string _endereco, Produto _estoque);
+};
 
 #endif
