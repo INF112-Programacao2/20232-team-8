1c6ef90e3d3cd3f25999d434a86fc5a3f0abbd75
maryanacosta <maryanacosta883@gmail.com>
2023-11-07 19:44:56 -0300

    criação das classes produto e vacina

--- Produto.h
+++ Produto.h
@@ -1,7 +1,12 @@
 #ifndef INF112_PRODUTO_H
 #define INF112_PRODUTO_H
 
-
-
+class Produto {
+private:
+    int _quantidade;
+public:
+    Produto(int quantidade);
+    int get_quantidade();  
+};
 
 #endif
--- Vacina.h
+++ Vacina.h
@@ -1,7 +1,17 @@
 #ifndef INF112_VACINA_H
 #define INF112_VACINA_H
 #include <string>
+#include "produto.h"
 
-
-
+class Vacina : public Produto {
+private:
+    std::string _nome;
+    std::string _fabricante;
+    std::string _lote;
+public:
+    Vacina(std::string nome, std::string fabricante, std::string _lote, int quantidade);
+    std::string get_nome();
+    std::string get_fabricante();
+    std::string get_lote();   
+};
 #endif
