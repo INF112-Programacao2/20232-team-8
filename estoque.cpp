0b3b7c24d8f8e7fc5256b71d7c14e61353b469f7
maryanacosta <maryanacosta883@gmail.com>
2023-11-19 16:20:16 -0300

    atualização da classe estoque - metodos set e get

--- estoque.cpp
+++ estoque.cpp
@@ -1,1 +1,48 @@
+#include <iostream>
+#include "estoque.h"
 
+//construtor
+Estoque::Estoque(std::string nome, int quantidade, std::string validade, std::string data_recebimento, std::string lote) :
+    _nome(nome), _quantidade(quantidade), _validade(validade), _data_recebimento(_data_recebimento), _lote(lote) {}
+
+//metodos set
+void Estoque::set_nome(std::string new_nome){
+    _nome = new_nome;
+}
+
+void Estoque::set_quantidade(int new_quantidade){
+    _quantidade = new_quantidade;
+}
+
+void Estoque::set_validade(std::string new_validade){
+    _validade = new_validade;
+}
+
+void Estoque::set_data_recebimento(std::string new_data_recebimento){
+    _data_recebimento = new_data_recebimento;
+}
+
+void Estoque::set_lote(std::string new_lote){
+    _lote = new_lote;
+}
+
+//metodos get
+std::string Estoque::get_nome(){
+    return _nome;
+}
+
+int Estoque::get_quantidade(){
+    return _quantidade;
+}
+
+std::string Estoque::get_validade(){
+    return _validade;
+}
+
+std::string Estoque::get_data_recebimento(){
+    return _data_recebimento;
+}
+
+std::string Estoque::get_lote(){
+    return _lote;
+}
--- posto.cpp
+++ posto.cpp
@@ -1,6 +1,6 @@
-#include "Posto.h"
-#include "Estoque.h"
-#include "Vacina.h"
+#include "posto.h"
+#include "estoque.h"
+#include "vacina.h"
 
 Posto::Posto(std::string nome, std::string endereco, Estoque estoque, Vacina vacina):
     _nome(nome), _endereco(endereco), _estoque(estoque), _vacina(vacina) {}
