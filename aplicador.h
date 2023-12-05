cd248969eeb9e45d572ebd3ecb4298310973d082
lilicesm <alicesm3012@gmail.com>
2023-12-05 18:20:15 -0300

    definindo operações do aplicador

--- aplicador.h
+++ aplicador.h
@@ -1,6 +1,7 @@
 #ifndef INF112_APLICADOR_H
 #define INF112_APLICADOR_H
 #include "usuario.h"
+#include "historico.h"
 #include <string>
 
 class Aplicador: public Usuario {
@@ -9,8 +10,12 @@ private:
 public:
     Aplicador(std::string login, std::string senha, std::string nome, std::string telefone, std::string email,
                 std::string cpf, std::string data_nascimento, std::string coren);
+    
     std::string get_coren();
     void set_coren(std::string coren);
+    void registrar_vacina();
+    void verificar_retorno();
+    void definir_retorno();
 };
 
 
